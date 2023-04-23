a9f9a1e99411abdd537977af648cbf3784be07ea
Jack Kerr <69304112+jckkrr@users.noreply.github.com>
2023-04-23 23:47:17 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,14 @@
 import streamlit as st
 
-st.write('hi hi')
+st.write('Bellingcat Hackathon Apr 2023: Accessibility Tool')
+
+st.write('# BOMBARDIER WATCH #####')
+st.write('by Jack Kerr (Group: Jackmaster)')
+st.write('')
+st.write('A tool to monitor the movements of business jets around the world.')
+st.write('Unfiltered crowd-sourced flight-tracking service ADS-B Exchange offers an excellent view into air traffice movement. But its only on very rare occassions that OSINT can be used to work out who is on board.')
+st.write('This tool provides one solution to that: bt showing which of these jets arrived or departed from the same locationon the same day.')
+st.write('Looks at all Bombardier, Gulfstream, Dassault and Embraer business jets.')
+st.write('Only data that shows the planes on the ground is examined in this app, as this shows the location to be a destination or origin.')
+st.write('This protype model uses data from one day, as recorded at the start of each minute.')
+

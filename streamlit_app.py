bca1d024f9f94bb394db3127d2193317a2905401
Jack Kerr <69304112+jckkrr@users.noreply.github.com>
2023-04-23 22:22:01 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,6 @@ st.write('Bellingcat Hackathon Apr 2023: Accessibility Tool')
 st.write('# BOMBARDIER WATCH #####')
 
 st.markdown("""---""") 
+
+df = pd.read_csv('dfMERGE.csv')
+st.dataframe(data=df)

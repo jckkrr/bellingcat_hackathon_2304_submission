a1bf2fcc98b21dad2f9190a61defa49d207ad555
Jack Kerr <69304112+jckkrr@users.noreply.github.com>
2023-04-23 23:35:37 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,1 @@
-import datetime
-from geopy.geocoders import Nominatim
-import pandas as pd
-import plotly.graph_objects as go
 import streamlit as st
-
-df = pd.read_csv('dfMERGE.csv')
-display_df = df[['hex', 'r', 'RegisteredOwners', 'Manufacturer', 'Type', 'lat_rnd', 'lon_rnd']]
-
-
